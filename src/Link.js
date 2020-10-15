981dbf6d828ef823557777ceab87fe7654ea4e73
Lisa Wagner <wagnerl@mskcc.org>
2020-10-15 14:23:15 -0400

    New icon Request Tracker, aligned items

--- src/Link.js
+++ src/Link.js
@@ -1,24 +1,25 @@
-import React from "react";
-import { withStyles } from "@material-ui/core";
-import Icon from "@material-ui/core/Icon";
+import React from 'react';
+import { withStyles } from '@material-ui/core';
+import Icon from '@material-ui/core/Icon';
 
-const Link = ({ classes, iconColor, title, icon, source, disabled }) =>
-  disabled ? (
-    <span className="disabled-link">
-      <Icon style={{ fontSize: 80 }}>{icon}</Icon>
-      <div className="title">{title}</div>
-    </span>
-  ) : (
-    <a target="_blank"  rel="noopener noreferrer" href={source} className={"link " + iconColor}>
-      <Icon style={{ fontSize: 80 }}>{icon}</Icon>
-      <div className="title">{title}</div>
-    </a>
-  );
+const Link = ({ classes, iconColor, title, icon, source, disabled, isFa }) =>
+    disabled ? (
+        <span className='disabled-link'>
+            <Icon style={{ fontSize: 80 }}>{icon}</Icon>
 
-const styles = theme => ({
-  title: {
-    margin: "0 auto"
-  }
+            <div className='title'>{title}</div>
+        </span>
+    ) : (
+        <a target='_blank' rel='noopener noreferrer' href={source} className={'link ' + iconColor}>
+            {isFa ? <Icon className={`icon fa fa-${icon}`} /> : <Icon className={`icon `}>{icon}</Icon>}
+            <div className='title'>{title}</div>
+        </a>
+    );
+
+const styles = (theme) => ({
+    title: {
+        margin: '0 auto',
+    },
 });
 
 export default withStyles(styles)(Link);
