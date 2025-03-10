13560d80fef1a8e6ea909509a928e7bf33007776
Ojasvi1329 <ojasvipatel113@gmail.com>
2025-03-10 15:40:11 -0400

    Making the URLs dynamic for dev environment

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import { MuiThemeProvider, createMuiTheme, withStyles } from '@material-ui/core/
 import IgoIcon from './igo-icon-orange.png';
 import './App.scss';
 
+// Check if running on `igodev.mskcc.org` (QA) or `igo.mskcc.org` (Prod)
+const isDev = window.location.hostname === 'igodev.mskcc.org';
+
+// Set dynamic URLs based on environment
+const sampleSubmissionURL = isDev ? 'https://igodev.mskcc.org/sample-submission' : 'https://igo.mskcc.org/sample-submission';
+const sampleQCURL = isDev ? 'https://igodev.mskcc.org/sampleQC/' : 'https://igo.mskcc.org/sample-qc/';
+const runQCURL = isDev ? 'https://igodev.mskcc.org/runQC' : 'https://igo.mskcc.org/runqc';
+const requestTrackerURL = isDev ? 'https://igodev.mskcc.org/request-tracker' : 'https://igo.mskcc.org/request-tracker';
+
 function App() {
   return (
     <div className="App">
@@ -16,12 +25,13 @@ function App() {
               <img className="icon" alt="igo logo" src={IgoIcon} />
               <div className="title">IGO Website</div>
             </a>
-            <Link title="Sample Submission" source="https://igo.mskcc.org/sample-submission" iconColor="blue" icon="cloud_upload" />
-            <Link title="Request Tracker" isFa={true} source="https://igo.mskcc.org/request-tracker" iconColor="magenta" icon="tasks" />
-            <Link title="Sample QC " source="https://igo.mskcc.org/sample-qc" iconColor="green" icon="check_circle" />
-            <Link title="Reservations " source="https://genomics.mskcc.org/reservations" iconColor="green" icon="insert_invitation" />
+            <Link title="Sample Submission" source={sampleSubmissionURL} iconColor="blue" icon="cloud_upload" />
+            <Link title="Request Tracker" source={requestTrackerURL} iconColor="magenta" icon="tasks" />
+            <Link title="Sample QC" source={sampleQCURL} iconColor="green" icon="check_circle" />
+            <Link title="Run QC" source={runQCURL} iconColor="orange" icon="assignment" />
+            <Link title="Reservations" source="https://genomics.mskcc.org/reservations" iconColor="green" icon="insert_invitation" />
             <Link title="Careers" source="https://genomics.mskcc.org/careers/" iconColor="violet" icon="business_center" />
-            <Link title="Swab 'n Seq Results" source="https://igo.mskcc.org/swabnseq/" iconColor="blue" icon="pie_chart" />
+            <Link title="Swab 'n Seq Results" source="https://igo.mskcc.org/swabnseq" iconColor="blue" icon="pie_chart" />
           </div>
         </div>
       </MuiThemeProvider>
@@ -45,31 +55,9 @@ const theme = createMuiTheme({
       main: '#DF4602',
       dark: '#C24D00',
     },
-
     textSecondary: '#e0e0e0',
   },
 });
-const styles = (theme) => ({
-  main: {
-    display: 'grid',
-  },
-  content: {
-    display: 'grid',
-    width: '70vw',
-    textAlign: 'center',
-    gridRowGap: '2em',
-    gridTemplateAreas: "'a b' 'c c'",
-  },
-  title: {
-    margin: '0 auto',
-  },
-});
 
-export default withStyles(styles)(App);
+export default App;
 
-// <Link
-//          // disabled={true}
-//          title="Sequencing/Run QC (IGO only)"
-//          source="https://igo.mskcc.org/run-qc"
-//          icon="developer_board"
-//        />
